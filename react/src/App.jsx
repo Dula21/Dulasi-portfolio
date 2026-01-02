bac8cd1de2fdc7b6cbe3b729a398f70bec8eb4a7
Dula21 <nethmadulasi15@gmail.com>
2026-01-02 07:57:29 +0400

    Added menues

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
+        } bg-[var(--bg-main)] text-[var(--text-main)]`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
